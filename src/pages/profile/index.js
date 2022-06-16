dfb15c23441e410fce1fd051f5f263b712879b6d
Bagus Pramajaya <gempong123@gmail.com>
2022-06-17 00:14:23 +0800

    create global style for button and input

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -4,12 +4,6 @@ import { Button, Form, Input, Select, Row, Col, Upload, message } from 'antd';
 import { CameraOutlined, LoadingOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
-const { TextArea } = Input;
-const getBase64 = (img, callback) => {
-	const reader = new FileReader();
-	reader.addEventListener('load', () => callback(reader.result));
-	reader.readAsDataURL(img);
-};
 
 const beforeUpload = (file) => {
 	const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
@@ -24,6 +18,7 @@ const beforeUpload = (file) => {
 		message.error('Image must smaller than 2MB!');
 	}
 
+	console.log(file);
 	return isJpgOrPng && isLt2M;
 };
 
@@ -44,11 +39,8 @@ export default function Profile() {
 		}
 
 		if (info.file.status === 'done') {
-			// Get this url from response in real world.
-			getBase64(info.file.originFileObj, (url) => {
-				setLoading(false);
-				setImageUrl(url);
-			});
+			setImageUrl(info.file.originFileObj);
+			console.log(info.file.originFileObj)
 		}
 	};
 
@@ -68,132 +60,104 @@ export default function Profile() {
 
 	return (
 		<div className='container'>
-			<Row>
-				<Col span={12} push={6}>
-					<Upload
-						name='avatar'
-						listType='picture-card'
-						className='avatar-uploader relative mt-10 mb-6 w-24 h-24 mx-auto block'
-						showUploadList={false}
-						action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
-						beforeUpload={beforeUpload}
-						onChange={handleChange}
+			<div className='update-profile-wrapper max-w-[568px] w-full mx-auto'>
+				<Upload
+					name='avatar'
+					listType='picture-card'
+					className='avatar-uploader relative mt-10 mb-6 w-24 h-24 mx-auto block'
+					showUploadList={false}
+					beforeUpload={beforeUpload}
+					onChange={handleChange}
+				>
+					{imageUrl ? (
+						<img
+							src={imageUrl}
+							alt='avatar'
+							style={{
+								width: '100%',
+							}}
+						/>
+					) : (
+						uploadButton
+					)}
+				</Upload>
+				<Form
+					layout='vertical'
+					form={form}
+					name='control-hooks'
+					onFinish={onFinish}
+				>
+					<Form.Item
+						className='mb-4'
+						name='name'
+						label='Nama*'
+						required={false}
+						rules={[
+							{
+								required: true,
+								message: 'Please input your Name!',
+							},
+						]}
 					>
-						{imageUrl ? (
-							<img
-								src={imageUrl}
-								alt='avatar'
-								style={{
-									width: '100%',
-								}}
-							/>
-						) : (
-							uploadButton
-						)}
-					</Upload>
-					<Form
-						layout='vertical'
-						form={form}
-						name='control-hooks'
-						onFinish={onFinish}
+						<Input placeholder='Nama' />
+					</Form.Item>
+					<Form.Item
+						className='mb-4'
+						name='city'
+						label='Kota*'
+						required={false}
+						rules={[
+							{
+								required: true,
+								message: 'Please input your City!',
+							},
+						]}
 					>
-						<Form.Item
-							name='name'
-							label='Nama'
-							rules={[
-								{
-									required: true,
-								},
-							]}
+						<Select placeholder='Pilih Kota' allowClear>
+							<Option value='bali'>Bali</Option>
+							<Option value='surabaya'>Surabaya</Option>
+							<Option value='jakarta'>Jakarta</Option>
+						</Select>
+					</Form.Item>
+					<Form.Item
+						className='mb-4'
+						name='address'
+						label='Alamat*'
+						required={false}
+						rules={[
+							{
+								required: true,
+								message: 'Please input your Address!',
+							},
+						]}
+					>
+						<Input placeholder='Alamat' />
+					</Form.Item>
+					<Form.Item
+						className='mb-6'
+						name='phone'
+						label='No Handphone*'
+						required={false}
+						rules={[
+							{
+								required: true,
+								message: 'Please input your Phone!',
+							},
+						]}
+					>
+						<Input placeholder='contoh: +628123456789' />
+					</Form.Item>
+					<Form.Item>
+						<Button
+							className='w-full btn-custom'
+							type='primary'
+							htmlType='submit'
 						>
-							<Input
-								className='rounded-2xl h-12'
-								placeholder='Nama'
-							/>
-						</Form.Item>
-						<Form.Item
-							name='kota'
-							label='Kota'
-							rules={[
-								{
-									required: true,
-								},
-							]}
-						>
-							<Select
-								className='input-city'
-								placeholder='Pilih Kota'
-								allowClear
-							>
-								<Option value='bali'>Bali</Option>
-								<Option value='surabaya'>Surabaya</Option>
-								<Option value='jakarta'>Jakarta</Option>
-							</Select>
-						</Form.Item>
-						<Form.Item
-							name='alamat'
-							label='Alamat'
-							rules={[
-								{
-									required: true,
-								},
-							]}
-						>
-							<TextArea
-								rows={4}
-								className='rounded-2xl h-20'
-								placeholder='Contoh: Jalan Ikan Hiu 33'
-							/>
-						</Form.Item>
-						<Form.Item
-							name='no handphone'
-							label='No Handphone*'
-							className='mt-4'
-							rules={[
-								{
-									required: true,
-								},
-							]}
-						>
-							<Input
-								className='rounded-2xl h-12'
-								placeholder='contoh: +628123456789'
-							/>
-						</Form.Item>
-						{/* <Form.Item
-              noStyle
-              shouldUpdate={(prevValues, currentValues) =>
-                prevValues.gender !== currentValues.gender
-              }
-            >
-              {({ getFieldValue }) =>
-                getFieldValue("gender") === "other" ? (
-                  <Form.Item
-                    name="customizeGender"
-                    label="Customize Gender"
-                    rules={[
-                      {
-                        required: true,
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-                ) : null
-              }
-            </Form.Item> */}
-						<Form.Item>
-							<Button
-								className='w-full rounded-2xl h-12'
-								type='primary'
-								htmlType='submit'
-							>
-								Simpan
-							</Button>
-						</Form.Item>
-					</Form>
-				</Col>
-			</Row>
+							Simpan
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
 		</div>
 	);
 }
