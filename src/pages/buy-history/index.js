ccfa2db8d8d9cc9264db7a38fccdfa2539e25afe
Bagus Pramajaya <gempong123@gmail.com>
2022-07-24 18:28:06 +0800

    add status to transaction

--- src/pages/buy-history/index.js
+++ src/pages/buy-history/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import React, { useEffect } from 'react';
-import { Row, Col, Skeleton, Pagination } from 'antd';
+import { Row, Col, Skeleton, Pagination, Tag } from 'antd';
 import { useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import moment from 'moment/moment.js';
@@ -87,20 +87,42 @@ export default function BuyHistory() {
 													{moment(i.transactionDate).format('DD MMM, kk:mm')}
 												</span>
 											</div>
+											{i.status === 1 && (
+												<Tag color='orange' className='mb-1'>
+													Pending
+												</Tag>
+											)}
+											{i.status === 2 && (
+												<Tag color='red' className='mb-1'>
+													Ditolak
+												</Tag>
+											)}
+											{i.status === 3 && (
+												<Tag color='blue' className='mb-1'>
+													Diterima
+												</Tag>
+											)}
+											{i.status === 4 && (
+												<Tag color='green' className='mb-1'>
+													Selesai
+												</Tag>
+											)}
 											<p className='mb-1 text-black text-sm'>{i.productResponse.name}</p>
 											<p className='mb-1 text-black text-sm'>
 												{currency(i.productResponse.price)}
 											</p>
 											<p className='mb-1 text-black text-sm'>Menawar {currency(i.offerPrice)} </p>
-											<a
-												href={`https://api.whatsapp.com/send?phone=${i.productResponse.userResponse.phone.replace(
-													/^0/,
-													'62'
-												)}`}
-												target='_blank'
-											>
-												Chat Penjual
-											</a>
+											{i.status === 4 && (
+												<a
+													href={`https://api.whatsapp.com/send?phone=${i.productResponse.userResponse.phone.replace(
+														/^0/,
+														'62'
+													)}`}
+													target='_blank'
+												>
+													Chat Penjual
+												</a>
+											)}
 										</div>
 									</div>
 								))}
